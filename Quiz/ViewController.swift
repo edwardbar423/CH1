d04f6beb7f58e27b1c7aa0d2af56d37e9f5ccae7
Andrew Barber <edwardbar423@gmail.com>
2016-10-04 01:08:07 -0400

    Finished Chapter 1 and started animations section before I remembered I needed to turn in chapter 1's work.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func questionButton(_ sender: AnyObject) {
+        
+        self.currentQuestionIndex = self.currentQuestionIndex + 1
+        if self.currentQuestionIndex == self.questions.count {
+            self.currentQuestionIndex = 0
+        }
+        
+        let question = self.questions[self.currentQuestionIndex]
+        self.questionLabel.text = question
+        self.answerLabel.text = "???"
+        animatedLabelTransitions()
+    }
+    
+    @IBAction func answerButton(_ sender: AnyObject) {
+        
+        let answer = self.answers[self.currentQuestionIndex]
+        self.answerLabel.text = answer
+        
+    }
+    
+    
+    var questions = [String]()
+    var answers = [String]()
+    var currentQuestionIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.questions = ["What is your name?", "What is your quest?", "What is your favorite color?"]
+        self.answers = ["Sir Galahad of Camelot", "I seek the Grail", "Blue, No, yel-"]
+        
+        self.questionLabel.text = self.questions[self.currentQuestionIndex]
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.questionLabel.alpha = 0.0
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func animatedLabelTransitions() {
+    
+        UIView.animate(withDuration: 2.3) {
+            self.questionLabel.alpha = 1.0
+        }
+    
+    }
+    
+    
 }
 
